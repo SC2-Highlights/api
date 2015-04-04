8a4192ea41751af7e7bbf8e8858fde349b816d51
Simon Hoskins <simonhosk@gmail.com>
2015-04-04 17:38:12 +0100

    Added more methods for highlights

--- controllers/highlightCtrl.js
+++ controllers/highlightCtrl.js
@@ -7,10 +7,32 @@ module.exports = function(router, dbClient) {
 
         query.on('end', function(result) {
             res.status(200).json(result.rows[0]);
-        })
+        });
+    }
+
+    var loadAll = function(req, res) {
+        var sql = 'SELECT * FROM highlights JOIN events USING (event_id)';
+        var query = dbClient.query(sql);
+
+        query.on('end', function(result) {
+            res.status(200).json(result.rows);
+        });
+    }
+
+    var loadAllEvent = function(req, res) {
+        var id = req.params.id;
+
+        var sql = 'SELECT * FROM highlights JOIN events USING (event_id) WHERE event_id = $1::int';
+        var query = dbClient.query(sql, [id]);
+
+        query.on('end', function(result) {
+            res.status(200).json(result.rows);
+        });
     }
 
     router.get('/:id', load);
+    router.get('/', loadAll);
+    router.get('/event/:id', loadAllEvent);
 
     return router;
 }
--- server.js
+++ server.js
@@ -9,6 +9,14 @@ dbHandler.connect(config.db.credentials);
 var app = express();
 var router = express.Router();
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 routing(app, router, dbHandler);
 
 app.listen(3000);
