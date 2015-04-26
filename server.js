00a5b9de2d04256776461b5f52a3af029d8c26ed
Simon Hoskins <simonhosk@gmail.com>
2015-04-26 14:08:53 +0100

    go

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var serviceManager = {
 var app = express();
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Origin', 'http://sc2hl');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
