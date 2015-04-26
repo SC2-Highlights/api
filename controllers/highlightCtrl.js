c6efcfcf6066dfca9bf025abe7d8e762487e635d
Simon Hoskins <simonhosk@gmail.com>
2015-04-26 12:13:43 +0100

    Rating save

--- controllers/highlightCtrl.js
+++ controllers/highlightCtrl.js
@@ -47,11 +47,41 @@ module.exports = function(serviceManager) {
 
     var rate = function(req, res) {
         var id = req.params.id;
-        var data = req.body.data;
+        var rating = req.body.data.rating;
 
-        console.log(req.cookies);
+        if(rating != 1 && rating != 2 && rating != 3 && rating != 4 && rating != 5){
+            res.status(400).json({"message":"failure"});
+            return;
+        }
 
-        res.cookie(id, data.rating);
+        if(req.cookies.hasOwnProperty(id)) {
+            if(req.cookies[id] == rating){
+                res.status(200).json({"message":"successs"});
+                return;
+            }
+
+            var sql = 'UPDATE highlights SET rate' + req.cookies[id] + ' = rate' + req.cookies[id] + ' - 1 WHERE highlight_id = $1::int';
+            serviceManager.dbClient.query(sql, [id]);
+        }
+
+        var sql = 'UPDATE highlights SET rate' + rating + ' = rate' + rating + ' + 1 WHERE highlight_id = $1::int';
+        serviceManager.dbClient.query(sql, [id]);
+
+        var sql = 'SELECT * FROM highlights WHERE highlight_id = $1::int';
+        var query = serviceManager.dbClient.query(sql, [id]);
+
+        query.on('end', function(result) {
+
+            var highlight = result.rows[0];
+
+            var voteCount = highlight.rate1 + highlight.rate2 + highlight.rate3 + highlight.rate4 + highlight.rate5;
+            var weight = highlight.rate1 + highlight.rate2 * 2 + highlight.rate3 * 3 + highlight.rate4 * 4 + highlight.rate5 * 5;
+            var newRating = (weight / voteCount).toFixed(1);
+
+            var sql = 'UPDATE highlights SET rating = $1 WHERE highlight_id = $2::int';
+        });
+
+        res.cookie(id, rating);
 
         res.status(200).json({"message":"success"});
     }
@@ -62,7 +92,7 @@ module.exports = function(serviceManager) {
     router.get('/', loadAll);
     router.get('/event/:id', loadAllEvent);
     router.get('/search/:criteria', search);
-    router.post('/:id/rate', rate);
+    router.put('/:id', rate);
 
     return router;
 }
