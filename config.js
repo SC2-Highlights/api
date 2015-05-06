50d43587eb1b93db15644c1ffd4ace18d2af2fdf
Simon Hoskins <simonhosk@gmail.com>
2015-05-06 21:34:49 +0100

    Change to db credentials

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
     db: {
         credentials: {
             user: 'sc2hl',
+            password: 'mbmwinzolvqa',
             host: '127.0.0.1',
             database: 'sc2hl',
             port: 5432,
