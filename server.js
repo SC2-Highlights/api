a4646039f7b688e88ffacc35558756f10bda4b0c
BlueHatbRit <myalt94@gmail.com>
2015-05-24 22:35:14 +0100

    Add next error handling for 404 and 500 errors

--- server.js
+++ server.js
@@ -36,7 +36,21 @@ app.use(cookieParser())
 
 routing(app, serviceManager);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 app.listen(config.port);
 console.log('listening on port: ' + config.port);
-
-
