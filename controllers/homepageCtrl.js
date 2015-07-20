e26f5eafee2929eb2b1de6594a436a00e2e55de7
Nikolai Gulatz <nikolai.gulatz@googlemail.com>
2015-07-20 23:45:44 +0200

    Improved homepage
    Added methods for retrieving top rated, most recent and random
    highlights

--- controllers/homepageCtrl.js
+++ controllers/homepageCtrl.js
@@ -21,10 +21,49 @@ module.exports = function(serviceManager) {
         });
     }
 
+    var loadTopHighlight = function(req, res) {
+    	var limit = req.params.limit;
+
+    	var sql = "SELECT highlight_id, title, date_added, yt_url, rating FROM highlights ORDER BY rating DESC LIMIT $1::int";
+
+    	var query = serviceManager.dbClient.query(sql, [limit]);
+
+        query.on('end', function(result) {
+            res.status(200).json(result.rows);
+        });
+    }
+
+    var loadRecentHighlight = function(req, res) {
+    	var limit = req.params.limit;
+
+    	var sql = "SELECT highlight_id, title, date_added, yt_url, rating FROM highlights ORDER BY date_added DESC LIMIT $1::int";
+
+    	var query = serviceManager.dbClient.query(sql, [limit]);
+
+        query.on('end', function(result) {
+            res.status(200).json(result.rows);
+        });
+    }
+
+    var loadRandomHighlight = function(req, res) {
+    	var limit = req.params.limit;
+
+    	var sql = "SELECT highlight_id, title, date_added, yt_url, rating FROM highlights ORDER BY random() LIMIT $1::int";
+
+    	var query = serviceManager.dbClient.query(sql, [limit]);
+
+        query.on('end', function(result) {
+            res.status(200).json(result.rows);
+        });
+    }
+
     var router = express.Router();
 
     router.get('/:id', loadPost);
     router.get('/', loadAllPost);
+    router.get('/highlight/top/:limit', loadTopHighlight);
+    router.get('/highlight/recent/:limit', loadRecentHighlight);
+    router.get('/highlight/random/:limit', loadRandomHighlight);
 
     return router;
 }
